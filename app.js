463d5fc5f653fa17d9a590878205e34ce9315d18
Kevin Smith <futuretense@gmail.com>
2022-11-09 12:14:23 -0800

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 
 const express = require('express');
 const app = express();
@@ -23,7 +28,7 @@ const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+// extra packages
 app.set('trust proxy', 1);
 app.use(rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -33,12 +38,14 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// extra packages
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+app.use('api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
-// app.get('/', (req, res) => {
-//   res.send('jobs api');
-// });
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
